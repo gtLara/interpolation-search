d2e9d8564b5ac4925b075c999de665e1fce525d7
GabPGomes <gabpgomes@gmail.com>
2021-01-26 19:08:51 -0300

    Update linear_search.asm

--- tinker/linear_search.asm
+++ tinker/linear_search.asm
@@ -3,38 +3,51 @@
 	##trecho com declaracao de variaveis##
 	######################################
 	
-	#GABRIEL P: sugiro separar as variaveis por trecho de codigo, 
-	#tipo: essas vao na funcao, e essas outras vao na main
+	#variaveis da main#
+	arr: .word 3 5 6 12 19 20 42 49 60 120 250 # 11 elementos   #
+	n: .word 11                               		    #tamanho do array
+	x: .word 5                                                  # numero a ser encontrado
+	hi: .word 0						    #limites do
+	lo: .word 0						    #sub-array
 	
-	array: .word 3 5 6 12 19 20 42 49 60 120 250 # 11 elementos
-	size: .word 11                               
-	key: .word 5                                 # numero a ser encontrado
-	i: .word 0                                   # posicao em uso no array
-	error_signal: .word -1
-	
+	#variaveis da funcao#
+	pos: .word 0                                                # posicao em uso no array
+	error_signal: .word -1					    #
+
 .text 
 	##############################################
 	##trecho com as instrucoes, o programa mesmo##
 	##############################################
 	
 	#buscando os valores na memoria#
+main:	
+	lw $a0, n                          #n = 11
+	lw $a1, x			   #x = 5
+	lw $a2, lo                         #lo = 0
+	lw $a3, hi                         #hi = 0
 	
-	lw $s0, i
-	lw $s1, size                  
-	lw $s2, key
-	lw $s7, error_signal
+	addi $a3 $a0 -1                    #hi = n - 1
 	
-for:
+	jal interpolation		   #Entra na funcao interpolation, argumentos x = 5, hi = n - 1, lo = 0
+					   
+	# imprime indice de chave se estiver no vetor e sinal de erro (-1) se nao#
+	li $v0, 1			    #escreve no registrador do sistema o tipo de chamada
+	move $a0, $s7                       #coloca o valor a ser escrito no argumento
+	syscall		
+
+	
+interpolation:
 	
 	# t0: variavel de testes
 	# t1: a[i]
 	# t2: endereco de a[i] ( i * 4 )
+	# t3: i
 	
 	#######################
 	##condicoes de parada##
 	#######################
 	
-	#sao os tres primeiros if's do while#
+	#sao os dois primeiros if's da funcao#
 	
 	slt $t0, $s0, $s1                   # se chegou ao fim do array:
 	beqz $t0, done                      # se (i < tamanho) == 0 (falso) , escapar
@@ -54,7 +67,7 @@ for:
 	
 	addi $s0, $s0, 1                    # i ++
 	
-	j for                               # retorna para label "for"
+	j interpolation                     # retorna para label "interpolation"	 	 	    
 	
 return_index:
 	
@@ -62,12 +75,3 @@ return_index:
 	add $s7, $0, $t3                    # escreve sobre sinal de retorno
 	 
 	j done
-	
-done:
-					    # imprime indice de chave se estiver no vetor
-					    #e sinal de erro (-1) se nao.#
-	li $v0, 1			    #escreve no registrador do sistema o tipo de chamada
-	move $a0, $s7                       #coloca o valor a ser escrito no argumento
-	syscall		 	 	    
-	
-	
